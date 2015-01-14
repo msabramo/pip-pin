946659a45c741ca5f79de6985026ae23441a558a
Mark Steve Samson <hello@marksteve.com>
2015-01-14 10:51:21 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+  setup(
+    name="pip-pin",
+    version="0.0.1",
+    author="Mark Steve Samson",
+    author_email="hello@marksteve.com",
+    license="MIT",
+    url="https://github.com/marksteve/pip-pin",
+    py_modules=['pip_pin'],
+  )
